f7dba53859896a361fc51436f0910f9e7b47b2c2
Turzhanskyi <turzhansky81@gmail.com>
2020-04-01 01:16:37 +0300

    episode 4: Model validations

--- app/models/item.rb
+++ app/models/item.rb
@@ -19,4 +19,6 @@
 #  index_items_on_price  (price)
 #
 class Item < ApplicationRecord
+  validates :price, numericality: { greater_than: 0, allow_nil: true }
+  validates :name, :description, presence: true
 end
