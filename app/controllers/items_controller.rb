095ed431a47258d1793adadf9fddc660aecac0a2
Turzhanskyi <turzhansky81@gmail.com>
2020-04-01 22:53:36 +0300

    episode 15: Filters in controllers

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,26 +1,25 @@
 # frozen_string_literal: true
 
 class ItemsController < ApplicationController
+  before_action :find_item, only:      %i[show edit update destroy]
+  before_action :check_if_admin, only: %i[edit update new create destroy]
+
   def index
     @items = Item.all
   end
 
   # /items/1  GET
   def show
-    unless @item = Item.where(id: params[:id]).first
-      render plain: 'Page not found', status: 404
-    end
+    render plain: 'Page not found', status: 404 unless @item
   end
 
-  # /items/new  GET
+  # /items/new GET
   def new
     @item = Item.new
   end
 
-  # /items/1/edit  GET
-  def edit
-    @item = Item.find(params[:id])
-  end
+  # /items/1/edit GET
+  def edit; end
 
   # /items  POST
   def create
@@ -28,31 +27,40 @@ class ItemsController < ApplicationController
     if @item.errors.empty?
       redirect_to item_path(@item)
     else
-      render :new
+      render 'new'
     end
   end
 
   # /items/1  PUT
   def update
-    @item = Item.find(params[:id])
     @item.update(item_params)
     if @item.errors.empty?
       redirect_to item_path(@item)
     else
-      render :edit
+      render 'edit'
     end
   end
 
   # /items/1  DELETE
   def destroy
-    @item = Item.find(params[:id])
     @item.destroy
-    redirect_to action: 'index'
   end
 
   private
 
   def item_params
     params.require(:item).permit(:price, :name, :weight, :real, :description)
+  end
+
+  def find_item
+    @item = Item.find(params[:id])
+  end
+
+  def check_if_admin
+    render_403 unless params[:admin]
+  end
+
+  def render_403
+    render file: 'public/403.html', status: 403
   end
 end
