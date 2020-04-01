6db0048e200f6372064ada733b0df85a69bb44d2
Turzhanskyi <turzhansky81@gmail.com>
2020-04-01 12:28:22 +0300

    episode 8: Params in controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,14 @@ class ItemsController < ApplicationController
     @items = Item.all
     render plain: @items.map { |i| "#{i.name}: #{i.price}" }.join(', ')
   end
+
+  def create
+    @items = Item.create(item_params)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:price, :name, :weight, :real, :description)
+  end
 end
