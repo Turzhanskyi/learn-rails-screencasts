6e5ecca1dc70ce4fdafc249ae5c47d50bcf0db40
Turzhanskyi <turzhansky81@gmail.com>
2020-04-04 16:18:02 +0300

    episode 21: has_and_belongs_to_many assotiation

--- app/models/item.rb
+++ app/models/item.rb
@@ -22,4 +22,6 @@
 class Item < ApplicationRecord
   validates :price, numericality: { greater_than: 0, allow_nil: true }
   validates :name, :description, presence: true
+
+  has_and_belongs_to_many :carts
 end
