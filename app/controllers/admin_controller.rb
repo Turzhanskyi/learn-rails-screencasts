c6a15b8695924b30085e7563a4bb944299e78c4a
Turzhanskyi <turzhansky81@gmail.com>
2020-04-06 18:53:08 +0300

    episode 26: layouts

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class AdminController < ApplicationController
+  before_action :check_if_admin
+  layout 'admin'
+
+  def users_count
+    @users_count = User.count
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_scope :user do
+    get 'users/sign_out' => 'devise/sessions#destroy'
+  end
+
   devise_for :users
-  get 'users/profile', as: 'user_root'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :items do
     get :upvote, on: :member
     get :expensive, on: :collection
   end
+
+  get 'admin/users_count' => 'admin#users_count'
 end
