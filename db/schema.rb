22911f5cce3144b667454f18754c97970593f8a7
Turzhanskyi <turzhansky81@gmail.com>
2020-04-06 16:51:28 +0300

    added devise

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_200_405_154_251) do
+ActiveRecord::Schema.define(version: 20_200_405_162_742) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -69,5 +69,18 @@ ActiveRecord::Schema.define(version: 20_200_405_154_251) do
     t.string 'login'
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
+    t.string 'email', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.integer 'sign_in_count', default: 0, null: false
+    t.datetime 'current_sign_in_at'
+    t.datetime 'last_sign_in_at'
+    t.inet 'current_sign_in_ip'
+    t.inet 'last_sign_in_ip'
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['login'], name: 'index_users_on_login', unique: true
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
 end
